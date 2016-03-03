0f6c54ae661d20095cca1c429dc91208124d34cf
rikardolajos <rikard.olajos@gmail.com>
2016-03-03 15:53:37 +0100

    Add texture_activate()

--- src/texture.h
+++ src/texture.h
@@ -10,6 +10,7 @@ struct Texture {
 	int width, height, depth;
 };
 
+void texture_activate(Texture* t);
 int texture2D_from_ex5(Texture* t, const char* file_path);
 int texture3D_from_ex5(Texture* t, const char* file_path);
 
