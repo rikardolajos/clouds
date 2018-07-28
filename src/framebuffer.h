d9fb965f7ea778bf093e46055c01ff07c2100567
Rikard Olajos <rikard.olajos@gmail.com>
2018-07-28 22:31:30 +0200

    Add dependencies and resource to the repo

--- src/framebuffer.h
+++ src/framebuffer.h
@@ -11,6 +11,6 @@ struct Framebuffer {
 
 void framebuffer_scene_init(Framebuffer* f, int screen_width, int screen_height);
 void framebuffer_cloud_init(Framebuffer* f, int screen_width, int screen_height);
-void framebuffer_pingpong_init(Framebuffer* f, int screen_width, int screen_height);
+void framebuffer_pingpong_init(Framebuffer f[2], int screen_width, int screen_height);
 
 #endif
